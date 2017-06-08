c1ad0b3d2335b07282cd83730dc613b888212ae9
gabriel.cuesta.contractor <gabriel.cuesta.contractor@bbva.com>
2017-06-08 09:19:33 +0200

    first commit

--- StringObfuscation/ViewController.swift
+++ StringObfuscation/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelTxt: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let variable = "Hello world!"
+        
+        let o = Obfuscator(withSalt: [AppDelegate.self, NSObject.self, NSString.self])
+        let bytes = o.bytesByObfuscatingString(string: variable)
+        print(bytes) /*[110, 97, 80, 70, 65, 122, 87, 83, 67, 50, 33, 34]*/
+        
+        let value = o.reveal(key: bytes)
+        print(value) /*555555556ABC*/
+        
+        labelTxt.text = value
     }
 
     override func didReceiveMemoryWarning() {
